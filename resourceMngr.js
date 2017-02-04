686f70380cfb2fb854f7eb9d877b13ac50f779ed
Adam <aspsvensson@gmail.com>
2017-02-04 15:29:10 +0100

    rtwqed

--- resourceMngr.js
+++ resourceMngr.js
@@ -2,13 +2,23 @@ var resourceMngr = function(){
 
 }
 
-resourceMngr.prototype.loadResource = function(resource,cb){
+resourceMngr.prototype.loadResource = function(resource, cb){
   var xhttp = new XMLHttpRequest();
+  var that= this;
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-     cb(this.responseText);
+     that.parseMetaData(this.responseText,cb);
     }
   };
   xhttp.open("GET", resource, true);
   xhttp.send();
+}
+
+
+resourceMngr.prototype.parseMetaData = function(_data, cb){
+  var world = {}
+  var data = JSON.parse(_data);
+  console.log('metadata is now')
+  console.dir(data)
+  cb(world)
 }

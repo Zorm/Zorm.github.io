1d52a4eabd32cd84769e4e45494d9a1a02e9b6e9
Heroicsparta695 <Heroicsparta695@gmail.com>
2017-03-28 21:22:03 +0200

    y offset to x offset swap

--- map.js
+++ map.js
@@ -199,7 +199,7 @@ map.prototype.drawItem = function(item){
 }
 
 map.prototype.drawGestalt = function(gestalter){
-    this.ctx.drawImage(this.imageGestalter, 0, 0, 32, 32, (gestalter.x*64)+this.itemOffSet, (gestalter.y*64)+this.itemOffSet+4, 30, 32);
+    this.ctx.drawImage(this.imageGestalter, 0, 0, 32, 32, (gestalter.x*64)+this.itemOffSet+4, (gestalter.y*64)+this.itemOffSet, 30, 32);
 }
 
 //Reference!!!!

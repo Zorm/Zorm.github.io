9af1620a86846b92a3a935cdd16f5c16509733c4
Heroicsparta695 <Heroicsparta695@gmail.com>
2017-03-28 21:14:44 +0200

    Possition fix

--- map.js
+++ map.js
@@ -10,7 +10,7 @@ var map = function(mapData, tileSize, game, cb){
   this.ctx = game.ctx;
   this.items = [];
   this.gestalter = [];
-  this.itemOffSet = 5;
+  this.itemOffSet = 16;
   this.itemSize = 54;
 
   this.keyItems=

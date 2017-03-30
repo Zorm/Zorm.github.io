786ca34a5356eeb61856894ebb5ce08b2f725429
Adam <aspsvensson@gmail.com>
2017-03-30 18:38:30 +0200

    minor chaaange

--- gestalter.js
+++ gestalter.js
@@ -8,43 +8,6 @@ var gestalter = function(x, y, name, map){
 
 }
 
-/*
-gestalter.prototype.canIMove = function(direction){
-/*map.prototype.draw = function(){
-	  this.mapData.forEach(function(row, y){
-	    row.forEach(function(tile, x){
-	      this.drawTile(tile, x, y);
-	    }.bind(this));
-	  }.bind(this));
-	}*//*
-
-	switch(direction){
-		case 'up':
-			if (this.y>0){
-
-				this.y--;
-			}
-		break
-		case 'down':
-			if (this.y<this.map.mapData.length-1){
-				this.y++;
-		}
-		break
-		case 'right':
-			if (this.x<this.map.mapData[0].length-1){
-				this.x++;
-		}
-		break
-		case 'left':
-			if (this.x>0){
-				this.x--;
-		}
-		break
-	}
-
-}
-*/
-
 gestalter.prototype.move = function(direction){
 	console.log("anropar move: "+ direction);
 	//console.dir(this.map);
@@ -52,32 +15,24 @@ gestalter.prototype.move = function(direction){
 	//console.dir(this.map.mapData[0].length-1);
 	switch(direction){
 		case 'up':
-			if (this.y>0){
-				if(this.map.mapData[this.y-1][this.x] < 24){
-					this.y--;
-				}
+			if (this.y>0 && this.map.mapData[this.y-1][this.x] < 24){
+				this.y--;
 			}
 		break
 		case 'down':
-			if (this.y<this.map.mapData.length-1){
-				if(this.map.mapData[this.y+1][this.x] < 24){
-					this.y++;
-				}
-		}
+			if (this.y<this.map.mapData.length-1 && this.map.mapData[this.y+1][this.x] < 24){
+				this.y++;
+			}
 		break
 		case 'right':
-			if (this.x<this.map.mapData[0].length-1){
-				if(this.map.mapData[this.y][this.x+1] < 24){
-					this.x++;
-				}
-		}
+			if (this.x<this.map.mapData[0].length-1 && this.map.mapData[this.y][this.x+1] < 24){
+				this.x++;
+			}
 		break
 		case 'left':
-			if (this.x>0){
-				if(this.map.mapData[this.y][this.x-1] < 24){
-					this.x--;
-				}
-		}
+			if (this.x>0 && this.map.mapData[this.y][this.x-1] < 24){
+				this.x--;
+			}
 		break
 	}
 	this.map.events.forEach(function(event)

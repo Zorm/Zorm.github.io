7f203befe1e360217ab00c5f76892c386b3f738e
Adam <aspsvensson@gmail.com>
2017-03-30 16:19:34 +0200

    The bestest discovery evre made

--- gestalter.js
+++ gestalter.js
@@ -8,44 +8,74 @@ var gestalter = function(x, y, name, map){
 
 }
 
-gestalter.prototype.canIMove = function(location){
-	/*map.prototype.draw = function(){
-  this.mapData.forEach(function(row, y){
-    row.forEach(function(tile, x){
-      this.drawTile(tile, x, y);
-    }.bind(this));
-  }.bind(this));
-}*/
+gestalter.prototype.canIMove = function(direction){
+/*map.prototype.draw = function(){
+	  this.mapData.forEach(function(row, y){
+	    row.forEach(function(tile, x){
+	      this.drawTile(tile, x, y);
+	    }.bind(this));
+	  }.bind(this));
+	}*/
 
-
-
-}
-
-
-gestalter.prototype.move = function(dir){
-	console.log("anropar move: "+ dir);
-	console.dir(this.map);
-	console.dir(this.map.mapData.length-1);
-	console.dir(this.map.mapData[0].length-1);
-	switch(dir){
+	switch(direction){
 		case 'up':
 			if (this.y>0){
+
 				this.y--;
 			}
 		break
 		case 'down':
-		if (this.y<this.map.mapData.length-1){
-			this.y++;
+			if (this.y<this.map.mapData.length-1){
+				this.y++;
 		}
 		break
 		case 'right':
-		if (this.x<this.map.mapData[0].length-1){
-			this.x++;
+			if (this.x<this.map.mapData[0].length-1){
+				this.x++;
 		}
 		break
 		case 'left':
-		if (this.x>0){
-			this.x--;
+			if (this.x>0){
+				this.x--;
+		}
+		break
+	}
+
+}
+
+
+gestalter.prototype.move = function(direction){
+	console.log("anropar move: "+ direction);
+	console.dir(this.map);
+	console.dir(this.map.mapData.length-1);
+	console.dir(this.map.mapData[0].length-1);
+	switch(direction){
+		case 'up':
+			if (this.y>0){
+				if(this.map.mapData[this.y++][this.x] >= 24)
+					this.y++;
+				}
+			}
+		break
+		case 'down':
+			if (this.y<this.map.mapData.length-1){
+				if(this.map.mapData[this.y][this.x++] >= 24)
+					this.x++;
+				}
+		}
+		break
+		case 'right':
+			if (this.x<this.map.mapData[0].length-1){
+				if(this.map.mapData[this.y][this.x--] >= 24)
+					this.x--;
+				}
+		}
+		break
+		case 'left':
+			if (this.x>0){
+				if(this.map.mapData[this.y--][this.x] >= 24)
+					this.y--;
+				}
 		}
 		break
 	}
@@ -54,7 +84,5 @@ gestalter.prototype.move = function(dir){
 		if (this.x == event.x && this.y == event.y){
 			this.map.onEvent(event);
 		}
-	}.bind(this))	
+	}.bind(this))
 }
-
-

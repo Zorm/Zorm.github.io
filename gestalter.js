f15a322ea2f91610b3c44a4f12848df0bbe0e89a
Adam <aspsvensson@gmail.com>
2017-03-30 18:30:13 +0200

    säkert bra

--- gestalter.js
+++ gestalter.js
@@ -47,34 +47,34 @@ gestalter.prototype.canIMove = function(direction){
 
 gestalter.prototype.move = function(direction){
 	console.log("anropar move: "+ direction);
-	console.dir(this.map);
-	console.dir(this.map.mapData.length-1);
-	console.dir(this.map.mapData[0].length-1);
+	//console.dir(this.map);
+	//console.dir(this.map.mapData.length-1);
+	//console.dir(this.map.mapData[0].length-1);
 	switch(direction){
 		case 'up':
 			if (this.y>0){
-				if(this.map.mapData[this.y++][this.x] >= 24){
+				if(this.map.mapData[this.y-1][this.x] >= 24){
 					this.y--;
 				}
 			}
 		break
 		case 'down':
 			if (this.y<this.map.mapData.length-1){
-				if(this.map.mapData[this.y][this.x++] >= 24){
+				if(this.map.mapData[this.y+1][this.x] >= 24){
 					this.y++;
 				}
 		}
 		break
 		case 'right':
 			if (this.x<this.map.mapData[0].length-1){
-				if(this.map.mapData[this.y][this.x--] >= 24){
+				if(this.map.mapData[this.y][this.x+1] >= 24){
 					this.x++;
 				}
 		}
 		break
 		case 'left':
 			if (this.x>0){
-				if(this.map.mapData[this.y--][this.x] >= 24){
+				if(this.map.mapData[this.y][this.x-1] >= 24){
 					this.x--;
 				}
 		}
